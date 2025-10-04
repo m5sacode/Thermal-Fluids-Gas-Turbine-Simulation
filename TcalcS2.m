0a61a9cc933287bed165b26eddb261ba196323e6
ebasken <ebasken@utexas.edu>
2025-10-04 12:48:15 -0500

    testing a third order polynomial

---
+++ TcalcS2.m
@@ -0,0 +1,18 @@
+function T = TcalcS2(sb, yN2, yO2)
+
+a1 = 5e-9;
+b1 = -3e-5;
+c1 = .0846;
+d1 = 170.29;
+a2 = 5e-9;
+b2 = -3e-5;
+c2 = .0889;
+d2 = 182.55;
+
+err = 1;
+T = 272;
+while err > 1e-9
+    T = T+.01;
+    sbest = yN2*(a1*T^3+b1*T^2+c1*T+d1) + yO2*(a2*T^3+b2*T^2+c2*T+d2);
+    err = abs(sbest-sb);
+end
